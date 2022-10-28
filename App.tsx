abe459dc92682d1aeb8c744407bcb75cd77a8ecd
Kazeem Quadri <quadrikazeem01@gmail.com>
2022-10-28 08:26:25 +0100

    using google fonts

--- App.tsx
+++ App.tsx
@@ -2,8 +2,24 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigation from './src/navigations/navigator';
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_600SemiBold,
+  Montserrat_700Bold,
+} from '@expo-google-fonts/montserrat';
 
 export default function App() {
+  const [fontLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_600SemiBold,
+    Montserrat_700Bold,
+  });
+
+  if (!fontLoaded) {
+    return null;
+  }
+
   return (
     <>
       <StatusBar style='auto' />
@@ -11,12 +27,3 @@ export default function App() {
     </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
