f9c98993e7ede9bb2c73d476ac975e10f59729ec
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-01-05 21:44:55 +0100

    tailwind accent color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
-/** @type {import('tailwindcss').Config} */ 
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-  "./App.{js,jsx,ts,tsx}", 
-  "./src/screens/**/*.{js,jsx,ts,tsx}",
-  "./src/components/**/*.{js,jsx,ts,tsx}"
-],
+    './App.{js,jsx,ts,tsx}',
+    './src/screens/**/*.{js,jsx,ts,tsx}',
+    './src/components/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        accent: '#4D2DEC',
+      },
+    },
   },
   plugins: [],
-}
+};
