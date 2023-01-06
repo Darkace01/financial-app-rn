7fe5fdb186968cb1bb503a16d1df457edf2175b2
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-01-06 18:56:42 +0100

    spacing of transaction list

--- src/components/TransactionMinList.tsx
+++ src/components/TransactionMinList.tsx
@@ -6,11 +6,11 @@ const TransactionMinList = () => {
   const transItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <ScrollView
-      className={`bg-white rounded-r-lg rounded-l-lg rounded-b-none p-5 space-y-2`}
-      style={{ height: screenHeight / 2 }}
+      className={`bg-white rounded-r-3xl rounded-l-3xl rounded-b-none pt-5 px-5 space-y-2 `}
+      style={{ height: screenHeight / 2.11 }}
     >
       <Text className='text-slate-400 text-xs'>Today</Text>
-      <View>
+      <View className='pb-10'>
         {transItems.map((item, index) => {
           return <TransationItem key={index} />;
         })}
--- src/components/TransationItem.tsx
+++ src/components/TransationItem.tsx
@@ -3,17 +3,17 @@ import React from 'react';
 
 const TransationItem = () => {
   return (
-    <View className='flex flex-row justify-between'>
+    <View className='flex flex-row justify-between my-3'>
       <View>
-        <View className='bg-accent bg-opacity-0 rounded-full flex h-14 w-14 items-center justify-center'>
-          <Text className='text-white'>P</Text>
+        <View className='bg-[#EEF2F8] bg-opacity-0 rounded-full flex h-12 w-12 items-center justify-center'>
+          <Text className='text-accent'>P</Text>
         </View>
       </View>
-      <View>
+      <View className='flex justify-around'>
         <Text className='text-sm'>Transfer to Phillip</Text>
         <Text className='text-xs text-slate-400'>12:03 AM</Text>
       </View>
-      <View>
+      <View className='flex justify-center'>
         <Text>+N42,209</Text>
       </View>
     </View>
