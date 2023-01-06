4e6e871e6f740ba79bb4a271750ad93ead995b02
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-01-06 18:18:43 +0100

    transaction list

--- src/components/TransationItem.tsx
+++ src/components/TransationItem.tsx
@@ -5,15 +5,17 @@ const TransationItem = () => {
   return (
     <View className='flex flex-row justify-between'>
       <View>
-        <View className='bg-accent bg-opacity-25 p-2 rounded-full h-15 w-15'>
+        <View className='bg-accent bg-opacity-0 rounded-full flex h-14 w-14 items-center justify-center'>
           <Text className='text-white'>P</Text>
         </View>
       </View>
       <View>
-        <Text>Transfer to Phillip</Text>
-        <Text>12:03 AM</Text>
+        <Text className='text-sm'>Transfer to Phillip</Text>
+        <Text className='text-xs text-slate-400'>12:03 AM</Text>
       </View>
-      <View></View>
+      <View>
+        <Text>+N42,209</Text>
+      </View>
     </View>
   );
 };
