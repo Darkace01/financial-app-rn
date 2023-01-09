47e4600b27674a9bcb9453808d3bb881c20c2342
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-01-09 08:08:16 +0100

    Using a central asset file for all assets

---
+++ src/constants/assets.ts
@@ -0,0 +1,6 @@
+const assetsObject = {
+  mainRectangle: require(`../../assets/images/main-rectangle.png`),
+  otherRect: require(`../../assets/images/other-rect.png`),
+  defualtProfile: require(`../../assets/images/profile.jpg`),
+};
+module.exports = assetsObject;
--- src/screens/OnboardingScreens/Components/Modal.tsx
+++ src/screens/OnboardingScreens/Components/Modal.tsx
@@ -1,62 +1,54 @@
-import { View, Text, Image,Pressable } from 'react-native'
-import React from 'react'
-import { useNavigation } from '@react-navigation/native'
+import { View, Text, Image, Pressable } from 'react-native';
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+import assetsObject from '../../../constants/assets';
 
-const Modal = ({title, body, screen, image}) => {
+const Modal = ({ title, body, screen, image }) => {
   const navigation = useNavigation();
   return (
     <View className='w-[335px] flex items-center bg-white rounded-[40px] p-4 space-y-6'>
-      <Text className='text-center text-[#13085E] text-[25px] font-bold'>{title}</Text>
+      <Text className='text-center text-[#13085E] text-[25px] font-bold'>
+        {title}
+      </Text>
       <Text className='text-center text-[#13085E] max-w-[70%]'>{body}</Text>
       <View className='flex flex-row items-center space-x-1'>
         <Image
-            source={
-                image === 'first' ?
-                require(`../../../../assets/images/main-rectangle.png`)
-                :
-                require(`../../../../assets/images/other-rect.png`)
-            }
-            className={
-                image === 'first' ?
-                "w-[6px] h-[18px]"
-                :
-                "w-[6px] h-[6px]"
-            }
+          source={
+            image === 'first'
+              ? assetsObject.mainRectangle
+              : assetsObject.otherRect
+          }
+          className={image === 'first' ? 'w-[6px] h-[18px]' : 'w-[6px] h-[6px]'}
         />
         <Image
-            source={
-                image === 'second' ?
-                require(`../../../../assets/images/main-rectangle.png`)
-                :
-                require(`../../../../assets/images/other-rect.png`)
-            }
-            className={
-                image === 'second' ?
-                "w-[6px] h-[18px]"
-                :
-                "w-[6px] h-[6px]"
-            }
+          source={
+            image === 'second'
+              ? assetsObject.mainRectangle
+              : assetsObject.otherRect
+          }
+          className={
+            image === 'second' ? 'w-[6px] h-[18px]' : 'w-[6px] h-[6px]'
+          }
         />
         <Image
-            source={
-                image === 'third' ?
-                require(`../../../../assets/images/main-rectangle.png`)
-                :
-                require(`../../../../assets/images/other-rect.png`)
-            }
-            className={
-                image === 'third' ?
-                "w-[6px] h-[18px]"
-                :
-                "w-[6px] h-[6px]"
-            }
+          source={
+            image === 'third'
+              ? assetsObject.mainRectangle
+              : assetsObject.otherRect
+          }
+          className={image === 'third' ? 'w-[6px] h-[18px]' : 'w-[6px] h-[6px]'}
         />
       </View>
-      <Pressable onPress={()=>{navigation.navigate(screen)}} className='bg-[#2C14DD] h-[62px] w-[207px] rounded-3xl flex justify-center items-center'>
+      <Pressable
+        onPress={() => {
+          navigation.navigate(screen);
+        }}
+        className='bg-[#2C14DD] h-[62px] w-[207px] rounded-3xl flex justify-center items-center'
+      >
         <Text className='text-white text-base'>Next</Text>
       </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
