9f8da71267dcb2e9a1cd6d90076aba5552d74dd1
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-02-03 16:40:42 +0100

    category select list

--- src/modals/TransactionModal.tsx
+++ src/modals/TransactionModal.tsx
@@ -3,8 +3,20 @@ import React from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import NavigationTopBar from '../components/NavigationTopBar';
 import { fonts } from '../constants/globalStyles';
+import { SelectList } from 'react-native-dropdown-select-list';
 
 const TransactionModal = () => {
+  const [selected, setSelected] = React.useState('');
+
+  const data = [
+    { key: '1', value: 'Mobiles' },
+    { key: '2', value: 'Appliances' },
+    { key: '3', value: 'Cameras' },
+    { key: '4', value: 'Computers' },
+    { key: '5', value: 'Vegetables' },
+    { key: '6', value: 'Diary Products' },
+    { key: '7', value: 'Drinks' },
+  ];
   return (
     <SafeAreaView className='bg-white h-full mx-auto px-5'>
       <NavigationTopBar withFilter={false} text='Money In' />
@@ -13,14 +25,28 @@ const TransactionModal = () => {
           <Text className={`text-base font-[${fonts.font700}]`}>Title</Text>
           <View className='flex flex-row bg-themeGrey py-2 px-3 rounded-md items-center my-1'>
             <TextInput
-              placeholder='Search transactions'
+              placeholder='Food from bukka'
               className='ml-1 px-1 w-full'
             />
           </View>
         </View>
-        <View className='flex flex-row'>
+        <View className='flex flex-col'>
           <Text className={`text-base font-[${fonts.font700}]`}>Category</Text>
-          <View></View>
+          <View>
+            <SelectList
+              setSelected={(val) => setSelected(val)}
+              data={data}
+              save='value'
+              boxStyles={{
+                backgroundColor: '#F5F7FF',
+                borderRadius: 10,
+                height: 50,
+                width: '100%',
+                marginTop: 10,
+                borderWidth: 0,
+              }}
+            />
+          </View>
         </View>
       </View>
     </SafeAreaView>
