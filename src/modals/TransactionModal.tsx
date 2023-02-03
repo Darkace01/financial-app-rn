ae36d7992cf3a9126d1b9cd09b8de008e6a48392
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-02-03 15:45:37 +0100

    transaction in and out modal

--- src/modals/TransactionModal.tsx
+++ src/modals/TransactionModal.tsx
@@ -1,11 +1,29 @@
-import { View, Text } from 'react-native';
+import { View, Text, TextInput } from 'react-native';
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import NavigationTopBar from '../components/NavigationTopBar';
+import { fonts } from '../constants/globalStyles';
 
 const TransactionModal = () => {
   return (
-    <View>
-      <Text>TransactionModal</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full mx-auto px-5'>
+      <NavigationTopBar withFilter={false} text='Money In' />
+      <View>
+        <View>
+          <Text className={`text-base font-[${fonts.font700}]`}>Title</Text>
+          <View className='flex flex-row bg-themeGrey py-2 px-3 rounded-md items-center my-1'>
+            <TextInput
+              placeholder='Search transactions'
+              className='ml-1 px-1 w-full'
+            />
+          </View>
+        </View>
+        <View className='flex flex-row'>
+          <Text className={`text-base font-[${fonts.font700}]`}>Category</Text>
+          <View></View>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
