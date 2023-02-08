9220b38736ce786e7d4a8a6f4ceaec79c594a052
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-02-08 13:17:49 +0100

    transaction creation modal spacing

--- src/modals/TransactionModal.tsx
+++ src/modals/TransactionModal.tsx
@@ -20,7 +20,7 @@ const TransactionModal = () => {
   return (
     <SafeAreaView className='bg-white h-full mx-auto px-5'>
       <NavigationTopBar withFilter={false} text='Money In' />
-      <View>
+      <View className='space-y-5'>
         <View>
           <Text className={`text-base font-[${fonts.font700}]`}>Title</Text>
           <View className='flex flex-row bg-themeGrey py-2 px-3 rounded-md items-center my-1'>
