285051ab0f8cf085d42a69f5e89bbea0bdaf3339
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-02-08 13:17:58 +0100

    pull to refresh

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -1,14 +1,31 @@
-import { View, Text, Dimensions, ScrollView } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Dimensions,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
+import React, { useCallback, useState } from 'react';
 import TransationItem from './TransationItem';
 import CustomSearchBar from './CustomSearchBar';
 const screenHeight = Dimensions.get('window').height;
 const TransactionList = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
   const transItems = [1, 2, 3, 4, 5, 6];
   return (
     <ScrollView
       className={`bg-white rounded-r-3xl rounded-l-3xl rounded-b-none pt-5 px-5 space-y-2 `}
       style={{ height: screenHeight / 1.26 }}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
     >
       <View className='my-2'>
         <CustomSearchBar />
--- src/components/TransactionMinList.tsx
+++ src/components/TransactionMinList.tsx
@@ -1,13 +1,30 @@
-import { View, Text, Dimensions, ScrollView } from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  Dimensions,
+  ScrollView,
+  RefreshControl,
+} from 'react-native';
+import React, { useCallback, useState } from 'react';
 import TransationItem from './TransationItem';
 const screenHeight = Dimensions.get('window').height;
 const TransactionMinList = () => {
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(() => {
+    setRefreshing(true);
+    setTimeout(() => {
+      setRefreshing(false);
+    }, 2000);
+  }, []);
   const transItems = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   return (
     <ScrollView
       className={`bg-white rounded-r-3xl rounded-l-3xl rounded-b-none pt-5 px-5 space-y-2 `}
       style={{ height: screenHeight / 2.11 }}
+      refreshControl={
+        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+      }
     >
       <Text className='text-slate-400 text-xs'>Today</Text>
       <View className='pb-10'>
