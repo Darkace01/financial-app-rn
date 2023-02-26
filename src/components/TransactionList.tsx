4d086ac11a712683e94a7667b077c26a455ec27b
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-02-26 16:11:42 +0100

    Migrating transaction list to use hooks

--- src/components/TransactionList.tsx
+++ src/components/TransactionList.tsx
@@ -5,76 +5,34 @@ import {
   ScrollView,
   RefreshControl,
 } from 'react-native';
-import React, { useCallback, useState, useEffect } from 'react';
+import React, { useCallback } from 'react';
 import TransationItem from './TransationItem';
 import CustomSearchBar from './CustomSearchBar';
-import { getUserTransactions } from '../Helpers/Service/TransactionService';
-import { apiResponse, Transaction } from '../Helpers/Interfaces/apiResponse';
+import { Transaction } from '../Helpers/Interfaces/apiResponse';
 import { Toast } from 'react-native-toast-message/lib/src/Toast';
 import Loading from './Loading';
+import { useTransactionFetch } from '../hooks/useTransactionFetch';
 const screenHeight = Dimensions.get('window').height;
 const TransactionList = () => {
-  const [refreshing, setRefreshing] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
-  const initialTransactionItems: Transaction[] = [];
-  const [transactionItems, setTransactionItems] = useState<Transaction[]>(
-    initialTransactionItems
-  );
+  const { transactionItems, isLoading, error, setRefresh, refresh } =
+    useTransactionFetch();
   const onRefresh = useCallback(() => {
-    setRefreshing(true);
-    getUserTransactions()
-      .then((res: apiResponse<Transaction>) => {
-        if (res.hasError === false) {
-          setTransactionItems(res.data);
-        } else {
-          Toast.show({
-            type: 'error',
-            text1: 'Error',
-            text2: res.message,
-          });
-        }
-        setRefreshing(false);
-      })
-      .catch((error) => {
-        Toast.show({
-          type: 'error',
-          text1: 'Error',
-          text2: 'Something went wrong',
-        });
-        setRefreshing(false);
-      });
+    setRefresh(true);
   }, []);
 
-  useEffect(() => {
-    try {
-      setIsLoading(true);
-      getUserTransactions().then((res: apiResponse<Transaction>) => {
-        if (res.hasError === false) {
-          setTransactionItems(res.data);
-        } else {
-          Toast.show({
-            type: 'error',
-            text1: 'Error',
-            text2: 'Something went wrong',
-          });
-        }
-        setIsLoading(false);
-      });
-    } catch (error) {
-      Toast.show({
-        type: 'error',
-        text1: 'Error',
-        text2: 'Something went wrong',
-      });
-      setIsLoading(false);
-    }
-  }, []);
+  if (error) {
+    Toast.show({
+      type: 'error',
+      text1: 'Error',
+      text2: 'Something went wrong',
+    });
+  }
   return (
     <ScrollView
       className={`bg-white rounded-r-3xl rounded-l-3xl rounded-b-none pt-5 px-5 space-y-2 `}
       style={{ height: screenHeight / 1.26 }}
       refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        <RefreshControl refreshing={refresh} onRefresh={onRefresh} />
       }
     >
       <View className='my-2'>
