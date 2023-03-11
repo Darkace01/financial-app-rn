eba6143a34afbfc8f16581a21e0b96224ec741b8
Kazeem Quadri <quadrikazeem01@gmail.com>
2023-03-11 15:49:57 +0100

    remove unused asset files

--- src/constants/assets.ts
+++ src/constants/assets.ts
@@ -1,8 +1,5 @@
 const assetsObject = {
-  mainRectangle: require(`../../assets/images/main-rectangle.png`),
-  otherRect: require(`../../assets/images/other-rect.png`),
   defualtProfile: require(`../../assets/images/profile.jpg`),
-  splashImage: require(`../../assets/images/splashImage.png`),
   successMark: require(`../../assets/images/Successmark.png`),
 };
 module.exports = assetsObject;
